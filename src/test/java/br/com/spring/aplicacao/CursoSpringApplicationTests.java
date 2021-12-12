a3de1f83607eb2981df0684728767e59356a3428
heitormariano <heitor.eumesmo@gmail.com>
2021-12-12 12:43:10 -0300

    projeto criado

---
+++ src/test/java/br/com/spring/aplicacao/CursoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.spring.aplicacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
